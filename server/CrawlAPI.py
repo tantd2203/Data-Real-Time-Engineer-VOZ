e3de674e7f6e29ff6fc8a028e9eb1994e331d95b
tantd9 <tranduytan.220300@gmail.com>
2024-05-19 14:50:40 +0700

    fake api get random coment

---
+++ server/CrawlAPI.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+def read_excel_file(file_path):
+    df = pd.read_excel(file_path)
+    data = df.to_dict(orient='records')
+    return data
+
+@app.route('/random', methods=['GET'])
+def get_random_row():
+    data = read_excel_file('../data/dataset.xlsx')
+    if data:
+        random_row = random.choice(data)
+        return jsonify(random_row)
+    else:
+        return jsonify({"error": "No data found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
